b19973a5abd340c6ccfc45b9973a7dedfb3fa2b4
Manuela Danso-Fordjour <manuela7@live.unc.edu>
2021-11-08 13:48:42 -0500

    .get

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/app/users", (req, res) => {
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req,res) =>{
 	const stmt = db.prepare('SELECT * FROM userinfo WHERE id = ?' )
-	const output = stmt.run(req.params.id)
+	const output = stmt.get(req.params.id)
 	res.status(200).json(output);
 })
 
